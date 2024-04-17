5ce163b2e6846d5657559b1392412de6cd0496cf
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-04-17 14:50:46 +0300

    Add EnumerableExtensions: HasSingle, IsEmpty

---
+++ MediFix.SharedKernel/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace MediFix.SharedKernel.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static bool HasSingle<T>(this IEnumerable<T> sequence)
+    {
+        if (sequence is ICollection<T> list)
+        {
+            return list.Count == 1;
+        }
+
+        using var iterator = sequence.GetEnumerator();
+        return iterator.MoveNext() && !iterator.MoveNext();
+    }
+
+    public static bool HasSingle<T>(this IEnumerable<T> sequence, out T value)
+    {
+        if (sequence is IList<T> { Count: 1 } list)
+        {
+            value = list[0];
+            return true;
+        }
+
+        using var iterator = sequence.GetEnumerator();
+        if (iterator.MoveNext())
+        {
+            value = iterator.Current;
+            if (!iterator.MoveNext())
+            {
+                return true;
+            }
+        }
+
+        value = default(T);
+        return false;
+    }
+
+    public static bool IsEmpty<T>(this IEnumerable<T> sequence)
+    {
+        if (sequence is ICollection<T> list)
+        {
+            return list.Count == 0;
+        }
+
+        using var iterator = sequence.GetEnumerator();
+        return !iterator.MoveNext();
+    }
+}
