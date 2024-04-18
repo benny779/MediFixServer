d0827e478cc5d7b3a9e3c3ca7a0f27148023c389
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-04-18 14:33:11 +0300

    Add Generic repository

---
+++ MediFix.Infrastructure/Persistence/Repositories/Abstractions/Repository.cs
@@ -0,0 +1,54 @@
+﻿using MediFix.Application.Abstractions.Data;
+using MediFix.Domain.Core.Primitives;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediFix.Infrastructure.Persistence.Repositories.Abstractions;
+
+public abstract class Repository<TEntity, TId>(DbContext dbContext)
+    : IRepository<TEntity, TId>
+    where TEntity : Entity<TId>
+{
+    public async Task<Result<TEntity>> GetByIdAsync(TId id, CancellationToken cancellationToken = default)
+    {
+        var entity = await dbContext.Set<TEntity>().FindAsync([id], cancellationToken);
+
+        if (entity is null)
+        {
+            return Error.EntityNotFound<TEntity>(id);
+        }
+
+        return entity;
+    }
+    public async Task<Result> InsertAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<TEntity>().Add(entity);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+    public async Task<Result> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<TEntity>().Update(entity);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+    public async Task<Result> DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        dbContext.Set<TEntity>().Remove(entity);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+    public async Task<Result> DeleteByIdAsync(TId id, CancellationToken cancellationToken = default)
+    {
+        var rowsDeleted = await dbContext.Set<TEntity>()
+            .Where(l => l.Id!.Equals(id))
+            .ExecuteDeleteAsync(cancellationToken);
+
+        return rowsDeleted > 0 ? Result.Success() : Error.EntityNotFound<TEntity>(id);
+    }
+}
--- Medifix.Application/Abstractions/Data/IRepository.cs
+++ Medifix.Application/Abstractions/Data/IRepository.cs
@@ -1,10 +1,14 @@
-﻿namespace MediFix.Application.Abstractions.Data;
+﻿using MediFix.Domain.Core.Primitives;
+using MediFix.SharedKernel.Results;
 
+namespace MediFix.Application.Abstractions.Data;
 
-public interface IRepository
+public interface IRepository<TEntity, in TId>
+    where TEntity : Entity<TId>
 {
-}
-
-public interface IRepository<T> : IRepository
-{
+    Task<Result<TEntity>> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
+    Task<Result> InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<Result> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<Result> DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task<Result> DeleteByIdAsync(TId id, CancellationToken cancellationToken = default);
 }
