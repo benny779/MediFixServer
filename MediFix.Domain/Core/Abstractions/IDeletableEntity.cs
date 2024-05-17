2b2486096cbbc7aba07e829499e847535e3d2aa1
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-05-17 12:21:56 +0300

    Update IDeletableEntity DeleteedBy

--- MediFix.Domain/Core/Abstractions/IDeletableEntity.cs
+++ MediFix.Domain/Core/Abstractions/IDeletableEntity.cs
@@ -1,8 +1,10 @@
-﻿namespace MediFix.Domain.Core.Abstractions;
+﻿using MediFix.Domain.Users;
+
+namespace MediFix.Domain.Core.Abstractions;
 
 public interface IDeletableEntity
 {
-    Guid? DeletedBy { get; set; }
+    UserId? DeletedBy { get; set; }
     DateTime? DeletedOn { get; set; }
     bool IsDeleted => DeletedBy is not null;
 }
