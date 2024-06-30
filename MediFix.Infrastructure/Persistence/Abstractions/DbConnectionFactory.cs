4ee9a20e5ece957ae31a804651a0dfd88cb275a2
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-06-30 21:29:06 +0300

    Add IDbConnectionFactory and implementation

---
+++ MediFix.Infrastructure/Persistence/Abstractions/DbConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using MediFix.Application.Abstractions.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace MediFix.Infrastructure.Persistence.Abstractions;
+
+internal class DbConnectionFactory : IDbConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public DbConnectionFactory(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("MediFix")
+            ?? throw new InvalidOperationException("DbConnectionFactory: ConnectionString not found.");
+    }
+
+    public IDbConnection CreateOpenConnection()
+    {
+        IDbConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
+        return connection;
+    }
+}
---
+++ Medifix.Application/Abstractions/Data/IDbConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace MediFix.Application.Abstractions.Data;
+
+public interface IDbConnectionFactory
+{
+    IDbConnection CreateOpenConnection();
+}
