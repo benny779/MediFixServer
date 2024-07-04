059e20f9503c63bc9b94c4f85870b3980ee99a94
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-07-04 11:17:18 +0300

    Add FromTryCatchAsync to Result

--- MediFix.SharedKernel/Results/Result.cs
+++ MediFix.SharedKernel/Results/Result.cs
@@ -45,6 +45,31 @@ public class Result
         }
     }
 
+    public static async Task<Result> FromTryCatchAsync(Func<Task> action)
+    {
+        try
+        {
+            await action();
+            return Success();
+        }
+        catch (Exception ex)
+        {
+            return Failure(Error.Failure(code: "General.Exception", description: ex.Message));
+        }
+    }
+
+    public static async Task<Result<TResult>> FromTryCatchAsync<TResult>(Func<Task<TResult>> action)
+    {
+        try
+        {
+            return await action();
+        }
+        catch (Exception ex)
+        {
+            return Failure<TResult>(Error.Failure(code: "General.Exception", description: ex.Message));
+        }
+    }
+
     public static Result Success() => new(true, Error.None);
     public static Result<TResult> Success<TResult>(TResult value) => new(true, Error.None, value);
     public static Result Failure(Error error) => new(false, error);
