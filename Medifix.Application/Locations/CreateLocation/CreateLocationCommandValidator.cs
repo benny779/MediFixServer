bbc066f6f3dab4e569162521d6fa974efe9917e6
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-07-14 11:24:34 +0300

    Add CreateLocationCommandValidator

---
+++ Medifix.Application/Locations/CreateLocation/CreateLocationCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MediFix.Application.Locations.CreateLocation;
+
+public class CreateLocationCommandValidator 
+    : AbstractValidator<CreateLocationCommand>
+{
+    public CreateLocationCommandValidator()
+    {
+        RuleFor(location => location.Name)
+            .NotEmpty();
+
+        RuleFor(location => location.LocationType)
+            .IsInEnum();
+    }
+}
