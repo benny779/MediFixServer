14c0f69583134abc221391b19864d81ade108254
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-07-16 10:43:29 +0300

    Add Validation extensions: PhoneNumber and ConfirmPassword

---
+++ Medifix.Application/Extensions/Validation/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediFix.Application.Extensions.Validation.Validators;
+using System.Linq.Expressions;
+
+namespace MediFix.Application.Extensions.Validation;
+
+public static class Extensions
+{
+    public static IRuleBuilderOptions<T, string?> PhoneNumber<T>(this IRuleBuilder<T, string?> ruleBuilder)
+    {
+        return ruleBuilder
+            .SetValidator(new PhoneNumberValidator<T>())
+            .WithMessage("'{PropertyName}' is not a valid phone number.");
+    }
+
+    public static IRuleBuilderOptions<T, string> ConfirmPassword<T>(
+        this IRuleBuilder<T, string> ruleBuilder,
+        Expression<Func<T, string>> expression)
+    {
+        return ruleBuilder
+            .NotEmpty()
+            .MinimumLength(8)
+            .Equal(expression)
+            .WithMessage("'{PropertyName}' must be equal to '{ComparisonProperty}'.");
+    }
+}
