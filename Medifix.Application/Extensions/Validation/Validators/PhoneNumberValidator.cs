f156b61730ab8184d42b1b75f70d0a1fa33566d1
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-07-16 10:42:47 +0300

    Add PhoneNumberValidator

---
+++ Medifix.Application/Extensions/Validation/Validators/PhoneNumberValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace MediFix.Application.Extensions.Validation.Validators;
+
+public class PhoneNumberValidator<T> : PropertyValidator<T, string?>
+{
+    public override string Name => "PhoneNumberValidator";
+
+    public override bool IsValid(ValidationContext<T> context, string? value)
+    {
+        if (value is null)
+        {
+            return true;
+        }
+
+        if (value is { Length: < 8 or > 10 })
+        {
+            return false;
+        }
+
+        if (value.Any(c => !char.IsDigit(c)))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
