0e6e67e949cacff348381d86f5e5e063063f5e68
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-07-21 15:31:52 +0300

    Add SubCategoryIdMapper

---
+++ MediFix.Infrastructure/Persistence/StronglyTypedIdMappers/SubCategoryIdMapper.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using MediFix.Domain.Categories;
+using System.Data;
+
+namespace MediFix.Infrastructure.Persistence.StronglyTypedIdMappers;
+
+internal sealed class SubCategoryIdMapper : SqlMapper.TypeHandler<SubCategoryId>
+{
+    public override void SetValue(IDbDataParameter parameter, SubCategoryId? value)
+    {
+        parameter.Value = value is not null ? value.Value : DBNull.Value;
+    }
+
+    public override SubCategoryId Parse(object value)
+    {
+        return SubCategoryId.From((Guid)value);
+    }
+}
