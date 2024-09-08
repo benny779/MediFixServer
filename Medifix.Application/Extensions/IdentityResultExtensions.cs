564e39b99b403d07b731bf2f342b447e6adaf4b7
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-09-08 09:50:05 +0300

    Add IdentityResultExtensions.ToResult

--- Medifix.Application/Extensions/IdentityResultExtensions.cs
+++ Medifix.Application/Extensions/IdentityResultExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MediFix.Application.Extensions;
 
-public static class IdentityErrorExtensions
+public static class IdentityResultExtensions
 {
     public static ValidationError ToValidationError(this IEnumerable<IdentityError> identityErrors)
     {
@@ -13,5 +13,12 @@ public static class IdentityErrorExtensions
             .ToArray();
 
         return new ValidationError(errors);
+    }
+
+    public static Result ToResult(this IdentityResult identityResult)
+    {
+        return identityResult.Succeeded
+            ? Result.Success()
+            : identityResult.Errors.ToValidationError();
     }
 }
