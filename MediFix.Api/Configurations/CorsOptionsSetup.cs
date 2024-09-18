1ba9f85651f7bd04b5a0a9468f4fa47d5ed9a01c
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-09-18 20:15:09 +0300

    Configure CORS to allow origins from configuration

--- MediFix.Api/Configurations/CorsOptionsSetup.cs
+++ MediFix.Api/Configurations/CorsOptionsSetup.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
 namespace MediFix.Api.Configurations;
 
-internal class CorsOptionsSetup : IConfigureOptions<CorsOptions>
+internal class CorsOptionsSetup(IConfiguration configuration) : IConfigureOptions<CorsOptions>
 {
     public const string DefaultPolicyName = "CorsPolicy";
 
     public void Configure(CorsOptions options)
     {
+        var corsConfiguration = configuration
+            .GetRequiredSection(CorsConfiguration.SectionName)
+            .Get<CorsConfiguration>()!;
+
         options.AddPolicy(DefaultPolicyName, policyBuilder =>
         {
             policyBuilder
-                .AllowAnyOrigin()
+                .WithOrigins(corsConfiguration.AllowedOrigins)
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
     }
+}
+
+internal class CorsConfiguration
+{
+    public const string SectionName = "Cors";
+    public string[] AllowedOrigins { get; init; } = [];
 }
