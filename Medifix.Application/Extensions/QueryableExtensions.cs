ed451d4e8b3b69c5a066a7200f0e60528815d6e1
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-09-20 09:55:40 +0300

    Add QueryableExtensions.ResultToListAsync

---
+++ Medifix.Application/Extensions/QueryableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MediFix.SharedKernel.Results;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediFix.Application.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<Result<List<TSource>>> ResultToListAsync<TSource>(
+        this IQueryable<TSource> source,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await source.ToListAsync(cancellationToken);
+
+        if (!result.Any())
+        {
+            return Error.NotFound($"{nameof(TSource)}.NotFound", $"No ${nameof(TSource)} found.");
+        }
+
+        return result;
+    }
+}
